47c9085e929f9e9240bdb6758816ca3974131296
aleandradero <alexandra.randrade@senacsp.edu.br>
2025-02-26 11:05:09 -0300

    chore: create button compontents

---
+++ src/app/components/button/index.tsx
@@ -0,0 +1,14 @@
+import { TouchableOpacity, Text } from "react-native";
+import styles from "./styles";
+
+
+function Button(){
+    return(
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.label}></Text>
+        </TouchableOpacity>
+
+    )
+
+}
+export default Button;
